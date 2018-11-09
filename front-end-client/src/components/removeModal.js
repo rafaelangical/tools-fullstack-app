11aecc6214450842662f23f55f4046aba6291707
rafaelangical <rsnrafael@outlook.com>
2018-11-08 22:49:46 -0300

    Update

---
+++ front-end-client/src/components/removeModal.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './removeModal.css';
+import axios from 'axios';
+
+class RemoveModal extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      response: []
+    }
+  }
+
+  handleRemove() {
+    const id = this.props.id;
+    axios.delete(`http://localhost:3000/tools/${this.props.id}`)
+    this.props.closeModal();
+  }
+  
+  render() { 
+
+    if (!this.props.show) {
+      return null;
+    }
+
+    return (
+      <div className="App-modal-remove">
+        <div className="div-modal-remove-full">
+          <div className="div-modal-remove-top">
+            <h3><span>X</span> Remove Tool</h3>
+            <p>Are you sure you want to remove ?</p>
+          </div>
+          <div className="div-modal-remove-bottom">
+            <button onClick={() => this.props.closeModal()}>Cancel</button>
+            <button className="remove-tool-button" onClick={() => this.handleRemove()}>Yes, remove</button>
+            id: {this.props.id}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default RemoveModal;
